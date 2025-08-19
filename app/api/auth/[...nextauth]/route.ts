b1e1e0c95717445ed8f29aa5e35b94ab72b407cc
Manish Modanwal <manishmodanwal400@gmail.com>
2025-08-20 04:54:20 +0530

    added other providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,13 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
   CredentialsProvider({
-    // The name to display on the sign in form (e.g. 'Sign in with...')
+   
     name: 'login with email',
-    // The credentials is used to generate a suitable form on the sign in page.
-    // You can specify whatever fields you are expecting to be submitted.
-    // e.g. domain, username, password, 2FA token, etc.
-    // You can pass any HTML attribute to the <input> tag through the object.
+    
     credentials: {
       username: { label: "Username", type: "text", placeholder: "jsmith" },
       password: { label: "Password", type: "password" }
@@ -31,6 +29,11 @@ const handler = NextAuth({
         return null;
     }
     }
+  })
+  ,
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
   })
 ]
 }
