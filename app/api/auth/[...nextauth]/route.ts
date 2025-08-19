b9f1f1fb355681c4e2b7c5398c8d2db5d3b60193
Manish Modanwal <manishmodanwal400@gmail.com>
2025-08-20 04:52:24 +0530

    added credentials providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,39 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
 
-const handler = NextAuth({}
+const handler = NextAuth({
+    providers: [
+  CredentialsProvider({
+    // The name to display on the sign in form (e.g. 'Sign in with...')
+    name: 'login with email',
+    // The credentials is used to generate a suitable form on the sign in page.
+    // You can specify whatever fields you are expecting to be submitted.
+    // e.g. domain, username, password, 2FA token, etc.
+    // You can pass any HTML attribute to the <input> tag through the object.
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: { label: "Password", type: "password" }
+    },
+    
+    async authorize(credentials, req) {
+        const username=credentials?.username;
+        const password=credentials?.password;
+        console.log("username:", username);
+        console.log("password:", password);
+       
+      const user={
+        username:"manish",
+        password:"123456"
+    }
+    if(user){
+        return user;
+    }else{
+        return null;
+    }
+    }
+  })
+]
+}
  );
 
 
